433c11d27d9a967b7443bfc3f28ee4adb616a6e2
dbabbj <70219804+dbabbj@users.noreply.github.com>
2021-08-30 13:29:08 -0600

    testing CI

--- sentinel_flutter_plugin/test/sentinel_flutter_plugin_test.dart
+++ sentinel_flutter_plugin/test/sentinel_flutter_plugin_test.dart
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await SentinelFlutterPlugin.platformVersion, '41');
+    expect(await SentinelFlutterPlugin.platformVersion, '40');
   });
 }
